dd19833276747fb14b3b1b33c59c352d76b5ce3c
Brandon <branpm@gmail.com>
2018-09-18 22:13:08 -0700

    Added a method to show the grid

--- src/com/github/sirmorde/connectfour/Main.java
+++ src/com/github/sirmorde/connectfour/Main.java
@@ -3,8 +3,21 @@ package com.github.sirmorde.connectfour;
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
-        System.out.println("Hello world!");
-        System.out.println("Here is another change!");
+        char[][] connectFourGrid = new char[6][7];
+        System.out.println("Welcome to Connect 4!\n");
+        showGrid(connectFourGrid);
+    }
+
+    public static void showGrid(char[][] connectFourGrid) {
+        for (int i = 0; i < connectFourGrid.length; i++) {
+            for (int j = 0; j < connectFourGrid[0].length; j++) {
+                System.out.print("|" + connectFourGrid[i][j]);
+            }
+            System.out.println("|");
+        }
+        for (int i = 0; i < connectFourGrid.length+1; i++) {
+            System.out.print("==");
+        }
+
     }
 }
