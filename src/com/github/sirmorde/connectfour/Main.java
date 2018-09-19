380f4fe78299ec0d94a583d44d098265c01771e4
Brandon <branpm@gmail.com>
2018-09-18 21:38:38 -0700

    Initial commit

---
+++ src/com/github/sirmorde/connectfour/Main.java
@@ -0,0 +1,9 @@
+package com.github.sirmorde.connectfour;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+        System.out.println("Hello world!");
+    }
+}
