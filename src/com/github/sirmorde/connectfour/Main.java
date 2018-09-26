a4785a348b8cd0cd5143447802b42ab469813eaf
Brandon <branpm@gmail.com>
2018-09-25 22:15:20 -0700

    Completed Features:    - Add functionality to drop chip (9/25)    - Added list to keep track of which row in the chosen column to drop a chip in (9/25)    - Alternate between player's turns (9/25)    - Display game instructions (9/25)
    Commit Notes
       9/25:
        - Changed hashmap to linkedhashmap to preserve order in which help commands appear
        - Renamed "reset" command to "clear"
        - Renamed "reset score" command to "reset"
        - Added list "nextEmpty" to keep track of which row in the chosen column to populate
        - Alternate between player's turns
        - Offset user inputted column by 1 in order to refer to the corresponding 0-indexed column
        - Refactored and removed unused code
        - Display game instructions
        - If less than 8 turns, no need to check for win yet

--- src/com/github/sirmorde/connectfour/Main.java
+++ src/com/github/sirmorde/connectfour/Main.java
@@ -3,16 +3,10 @@ GitHub Repository: https://github.com/SirMorde/connect-four-command-line-java
 Author: Brandon Ma (prithvichakra@gmail.com)
 
    To Do:
-   - Display game instructions
-   - Add functionality to drop chip
-   - Validate proper input for columns (Only allow 1-7)
+   - Fill in checkWin function
    - Check for horizontal win
    - Check for vertical win
    - Check for diagonal win
-
-   Edge cases:
-   - If less than 8 turns, no need to check for win yet
-   - If whole board is filled but no winner, reset the game
 
    Completed Features:
    - Support for "restart" and "exit"
@@ -21,13 +15,31 @@ Author: Brandon Ma (prithvichakra@gmail.com)
    - After a player wins, display play again text
    - Keep Red and Blue score
    - Reset score
-    */
+   - If whole board is filled but no winner, reset the game
+   - Validate proper input for columns (Only allow 1-7)
+   - Add functionality to drop chip (9/25)
+   - Added list to keep track of which row in the chosen column to drop a chip in (9/25)
+   - Alternate between player's turns (9/25)
+   - Display game instructions (9/25)
+
+   Commit Notes
+   9/25:
+    - Changed hashmap to linkedhashmap to preserve order in which help commands appear
+    - Renamed "reset" command to "clear"
+    - Renamed "reset score" command to "reset"
+    - Added list "nextEmpty" to keep track of which row in the chosen column to populate
+    - Alternate between player's turns
+    - Offset user inputted column by 1 in order to refer to the corresponding 0-indexed column
+    - Refactored and removed unused code
+    - Display game instructions
+    - If less than 8 turns, no need to check for win yet
+*/
 
 package com.github.sirmorde.connectfour;
 
 import java.util.Scanner;
 import java.util.Map;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.lang.*;
 
 public class Main {
@@ -43,24 +55,25 @@ public class Main {
     static String currentTurn = "INIT";
     static String userInput = "INIT";
 
-
-    // game board
+    // game board variables
     static char[][] connectFourBoard;
+    static int[] nextEmpty;
 
     // scanner used to read input
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        initializeBoard();
 
         // Initialize Help instructions
-        Map<String, String> helpCommands = new HashMap<String, String>();
-        helpCommands.put("help","Prints instructions");
-        helpCommands.put("reset","Clears the game board");
-        helpCommands.put("reset score","Resets the score and game board");
+        Map<String, String> helpCommands = new LinkedHashMap<String, String>();
+        helpCommands.put("help","Prints list of commands");
+        helpCommands.put("instructions","Prints game instructions");
+        helpCommands.put("reset","Resets the score and clears the game board");
+        helpCommands.put("clear","Clears the game board");
         helpCommands.put("exit","Closes the Game");
 
-        while (true) {
+        while(true) {
+            initializeBoard();
             System.out.println("Welcome to Connect 4! Type help for list of commands");
             showBoard(connectFourBoard);
 
@@ -78,6 +91,7 @@ public class Main {
                 System.out.println(currentTurn + " player's turn - Choose a column (1-7) or type a command. (Type help for list of commands)");
                 userInput = scanner.nextLine().toLowerCase().trim();
 
+                // Validate user input for one of the valid commands.
                 if (userInput.equals("help")) {
                     for (Map.Entry<String, String> entry : helpCommands.entrySet()) {
                         String key = entry.getKey();
@@ -86,85 +100,113 @@ public class Main {
                     }
                     System.out.println();
                 }
-                else if (userInput.equals("instructions")) {
-                    System.out.println("Put instructions here");
+                else if(userInput.equals("instructions")) {
+                    System.out.println("Be the first player to get four of your colored chips in a row - horizontally, vertically, or diagonally.");
                 }
-                else if (userInput.equals("reset")) {
+                else if(userInput.equals("reset")) {
+                    initializeScore();
                     initializeBoard();
                     showBoard(connectFourBoard);
                 }
-                else if (userInput.equals("reset score")) {
-                    initializeScore();
+                else if(userInput.equals("clear")) {
+                    initializeBoard();
                     showBoard(connectFourBoard);
                 }
-                else if (userInput.equals("exit")) {
+                else if(userInput.equals("exit")) {
                     System.exit(0);
                 }
                 else {
 
-                    // Validate input for 1-7
+                    // Validate user input for 1-7
                     try{
-                        int chosenColumn = Integer.parseInt(userInput);
-                        if (chosenColumn < 1 || chosenColumn > 7) {
+
+                        // Note:Offset chosenColumn by 1 since we refer to columns as index 0-6
+                        int chosenColumn = Integer.parseInt(userInput)-1;
+                        if(chosenColumn < 0 || chosenColumn > 6) {
                             System.out.println("ERROR: Input out of range. Please enter 1-7.");
                             continue;
                         }
+                        else if(nextEmpty[chosenColumn] < 0) {
+                            System.out.println("ERROR: The chosen column is full! Please choose a different column");
+                            continue;
+                        }
+                        else {
+                            connectFourBoard[nextEmpty[chosenColumn]][chosenColumn] = currentTurn.charAt(0);
+                            if(numOfTurns > 8 && checkWin(connectFourBoard)) {
+                                numOfTurns++;
+                                showBoard(connectFourBoard);
 
-                        // else if column is full. If it is, display an error message
+                                if(currentTurn == "Blue") {
+                                    blueWins++;
+                                }
+                                else {
+                                    redWins++;
+                                }
+                                System.out.println(currentTurn + " wins!");
+                                break;
+                            }
 
-                        // If win
-                        // Display win and break
-                        //System.out.println(currentTurn + " wins!");
-                        //break;
-
-                        // If board is full, restart game.
-                        if (numOfTurns == (ROW*COLUMN)) {
-                            System.out.println("Tie game! No one wins :(");
-                            break;
+                            nextEmpty[chosenColumn]--;
                         }
                     }
-                    catch (Exception e) {
+                    catch(Exception e) {
                         System.out.println("ERROR: Invalid input");
                         continue;
                     }
+
+                    // Toggle between player's turn
+                    if(currentTurn == "Blue") {
+                        currentTurn = "Red";
+                    }
+                    else {
+                        currentTurn = "Blue";
+                    }
+
                     numOfTurns++;
                     showBoard(connectFourBoard);
+
+                    // If board is full but no one has won, restart the game.
+                    if(numOfTurns == (ROW*COLUMN+1)) {
+                        System.out.println("Tie game! No one wins :(");
+                        break;
+                    }
                 }
             }
 
-            while (true) {
+            while(true) {
                 System.out.println("Play again? (y/n)");
                 userInput = scanner.nextLine().toLowerCase().trim();
                 if(userInput.equals("y") || userInput.equals("yes")) {
                     break;
                 }
-                else if (userInput.equals("n") || userInput.equals("no")) {
+                else if(userInput.equals("n") || userInput.equals("no")) {
                     System.out.print("Exiting...");
                     System.exit(0);
                 }
                 else {
-                    System.out.println("Invalid input. Please enter yes/y or no/n");
+                    System.out.println("Invalid input. Please enter y/yes or n/no");
                 }
             }
 
         }
     }
 
-    public static void initializeBoard() {
+    private static void initializeBoard() {
         System.out.println("Clearing board...");
         numOfTurns = 1;
         connectFourBoard = new char[ROW][COLUMN];
+
+        // Initialize the array containing the next available spot to drop a chip for each column
+        nextEmpty = new int[COLUMN];
+        for(int i = 0; i < COLUMN; i++) {
+            nextEmpty[i] = ROW-1;
+        }
     }
 
-    public static void initializeScore() {
-        System.out.println("Clearing score...");
+    private static void initializeScore() {
+        System.out.println("Resetting score...");
         redWins = 0;
         blueWins = 0;
-    }
-
-    public static String readInput(String playerTurn) {
-        userInput = scanner.nextLine();
-        return userInput;
     }
 
     public static void showBoard(char[][] connectFourBoard) {
@@ -182,17 +224,17 @@ public class Main {
         System.out.println();
     }
 
-    public static void checkWin(char[][] connectFourBoard) {
-        for (int i = 0; i < connectFourBoard.length; i++) {
-            for (int j = 0; j < connectFourBoard[0].length; j++) {
-                System.out.print("|" + connectFourBoard[i][j]);
-
-                // Check for horizontal win to the right
-                // Check for vertical win below
-                // Check for diagonal win to the bottom right
-                // Check for diagonal win to the bottom left
-            }
-        }
-
+    public static boolean checkWin(char[][] connectFourBoard) {
+//        for (int i = 0; i < connectFourBoard.length; i++) {
+//            for (int j = 0; j < connectFourBoard[0].length; j++) {
+//                System.out.print("|" + connectFourBoard[i][j]);
+//
+//                // Check for horizontal win to the right
+//                // Check for vertical win below
+//                // Check for diagonal win to the bottom right
+//                // Check for diagonal win to the bottom left
+//            }
+//        }
+        return true;
     }
 }
